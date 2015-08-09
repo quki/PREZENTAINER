6fae006dcfbdb97e45cbacd5a4d1e5cd9c12ce77
Quki <quki09@naver.com>
2015-08-09 16:31:29 +0900

    [TEST]

--- Android/PreZenTainer/src/com/puregodic/android/prezentainer/HomeActivity.java
+++ Android/PreZenTainer/src/com/puregodic/android/prezentainer/HomeActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-		// i am darkstars77
+		// i am darkstars77 i am quki
 		// how are you?
                 // shut up
 	Button homeStartBtn, homeLoadBtn;
