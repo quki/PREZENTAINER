f99b2e10a9062b2e55de5607a49aa34170c7c958
Quki <quki09@naver.com>
2015-08-09 18:27:26 +0900

    [RECOVERY]

--- Android/PreZenTainer/src/com/puregodic/android/prezentainer/HomeActivity.java
+++ Android/PreZenTainer/src/com/puregodic/android/prezentainer/HomeActivity.java
@@ -7,9 +7,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+    
+    // Home Activity
 
 	Button homeStartBtn, homeLoadBtn;
 
