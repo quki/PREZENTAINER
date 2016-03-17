fc4a256fa07c181bdcdcf8ef8f40bdf0d48f94d9
yohanchoi <xkth342@gmail.com>
2016-03-17 16:09:32 +0900

    <yohan> change URLs

--- Android/PreZenTainer/app/src/main/java/com/puregodic/android/prezentainer/network/NetworkConfig.java
+++ Android/PreZenTainer/app/src/main/java/com/puregodic/android/prezentainer/network/NetworkConfig.java
@@ -5,17 +5,21 @@ public class NetworkConfig {
     public static String URL_LOGIN = "http://54.64.87.31/logregi/index.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://54.64.87.31/logregi/index.php";
+    public static String URL_REGISTER = "http://cyh1704.dothome.co.kr/prezentainer/logregi/index.php";
 
     // Server insert data to DB url
-    public static String URL_INSERT = "http://54.64.87.31/tizen/inserting.php";
+    //public static String URL_INSERT = "http://54.64.87.31/tizen/inserting.php";
+    public static String URL_INSERT = "http://cyh1704.dothome.co.kr/prezentainer/insert.php";
+
 
     // Server fetch data from DB url
-    public static String URL_FETCH = "http://54.64.87.31/tizen/select.php";
+    //public static String URL_FETCH = "http://54.64.87.31/tizen/select.php";
+    public static String URL_FETCH ="http://cyh1704.dothome.co.kr/prezentainer/select.php";
 
-    public static String URL_FETCH_GRAPH = "http://54.64.87.31/tizen/second_select.php";
+   // public static String URL_FETCH_GRAPH = "http://54.64.87.31/tizen/second_select.php";
+    public static String URL_FETCH_GRAPH = "http://cyh1704.dothome.co.kr/prezentainer/second_select.php";
+   // public static String URL_DELETE = "http://54.64.87.31/tizen/delete.php";
 
-    public static String URL_DELETE = "http://54.64.87.31/tizen/delete.php";
-
+    public static String URL_DELETE = "http://cyh1704.dothome.co.kr/prezentainer/delete.php";
 
 }
